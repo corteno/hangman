df9d43d966c494baccaf2984b9a9148543d35541
tomi <tomi>
2017-02-14 16:31:17 +0100

    - CSS styling

--- js/app.js
+++ js/app.js
@@ -20,11 +20,11 @@ const difficulties = [
     }
 ];
 
-var currentState = "";
 var currentWord = "";
 var difficulty = "";
 var mistakesLeft;
 var guessedLetters = [];
+var lettersToGuess = 0;
 
 
 //----------------------
@@ -32,39 +32,7 @@ var guessedLetters = [];
 //----------------------
 
 var startingScreen = () => {
-    currentState = "start";
-    //Set focus on the input field
-    document.getElementById("word-input").focus();
-
-
-    //On enter press of this input
-    document.getElementById("word-input").onkeypress = function (e) {
-        if (!e) e = window.event;
-
-        var keyCode = e.keyCode || e.which;
-
-        if (keyCode == '13') {
-            var re = new RegExp("[A-Za-z]");
-
-            //Setting word to guess
-            currentWord = document.getElementById("word-input").value;
-            if (re.test(currentWord)) {
-                //console.log(currentWord);
-
-                //Setting difficulty
-                difficulty = document.querySelector('input[name="diff[]"]:checked').value;
-                //Clearing the div
-                clearApp();
-                //Starting the game
-                game();
-
-            } else {
-                console.log("Give me a proper word!");
-            }
-            return false;
-        }
-    }
-
+    createStartingScreen();
 
 };
 
@@ -85,16 +53,37 @@ var game = () => {
             var currentLetter = document.getElementById("guess-input").value;
 
             if (re.test(currentLetter)) {
-                if(!checkLetter(currentLetter)){
-                    mistakesLeft--;
+                if (!checkLetter(currentLetter)) {
                     updateMistakes();
                 }
             }
-            document.getElementById("guess-input").value = "";
+            if (document.getElementById("guess-input") != undefined) {
+                document.getElementById("guess-input").value = "";
+            }
+
             return false;
         }
 
     };
+
+
+};
+
+var gameOver = (state) => {
+    lettersToGuess = 0;
+
+
+    if (state == "win") {
+        clearApp();
+        createGameOverScene(state);
+
+        console.log("Congratulations! You've won!");
+    } else if (state == "loss") {
+        clearApp();
+        createGameOverScene(state);
+
+        console.log("You lost!");
+    }
 
 
 };
@@ -127,6 +116,88 @@ var setDifficulty = () => {
     });
 };
 
+var createStartingScreen = () => {
+    clearApp();
+    var startScreenWrapper = document.createElement("div");
+    var inputWrapper = document.createElement("div");
+    var label = document.createElement("label");
+    var span = document.createElement("span");
+    var input = document.createElement("input");
+    var first = true;
+
+    startScreenWrapper.id = "start-screen";
+    inputWrapper.id = "input-wrapper";
+    input.id = "word-input";
+    input.setAttribute("pattern", "[A-Za-z]");
+
+    label.className = "input-label";
+
+    span.innerHTML = "Give me a word";
+    span.className = "label-text";
+
+
+    label.appendChild(span);
+
+    inputWrapper.appendChild(input);
+    inputWrapper.appendChild(label);
+
+    startScreenWrapper.appendChild(inputWrapper);
+
+    difficulties.forEach((diff) => {
+        var radio = document.createElement("input");
+        var radioWrapper = document.createElement("div");
+        var radioLabel = document.createElement("label");
+
+        radioWrapper.className = "radio-wrapper";
+        radioLabel.innerHTML = diff.name;
+
+        radio.type = "radio";
+        radio.name = "diff[]";
+        radio.value = diff.name;
+        if (first) {
+            radio.checked = true;
+            first = false;
+        }
+
+        radioWrapper.appendChild(radio);
+        radioWrapper.appendChild(radioLabel);
+        startScreenWrapper.appendChild(radioWrapper);
+
+    });
+
+    appendToApp(startScreenWrapper);
+
+    //On enter press of this input
+    input.onkeypress = function (e) {
+        if (!e) e = window.event;
+
+        var keyCode = e.keyCode || e.which;
+
+        if (keyCode == '13') {
+            var re = new RegExp("[A-Za-z]");
+
+            //Setting word to guess
+            currentWord = document.getElementById("word-input").value;
+            if (re.test(currentWord)) {
+                //console.log(currentWord);
+
+                //Setting difficulty
+                difficulty = document.querySelector('input[name="diff[]"]:checked').value;
+                //Clearing the div
+                clearApp();
+                //Starting the game
+                game();
+
+            } else {
+                console.log("Give me a proper word!");
+            }
+            return false;
+        }
+    }
+
+    input.focus();
+};
+
 var createLetters = () => {
     var lettersWrapper = document.createElement("div");
     lettersWrapper.className = "letters-wrapper";
@@ -136,6 +207,7 @@ var createLetters = () => {
             var letterElement = document.createElement("div");
             letterElement.className = "letter";
             lettersWrapper.appendChild(letterElement);
+            lettersToGuess++;
         } else {                                                // If it's a space
             var spaceElement = document.createElement("div");
             spaceElement.className = "space-element";
@@ -162,19 +234,28 @@ var createGuessInput = () => {
 var checkLetter = (letter) => {
     var foundLetter = false;
 
+
     for (var i = 0; i < currentWord.length; i++) {
-        if (letter == currentWord[i] /* check if already typed this letter*/) {
-            console.log("correct guess");
+        if (letter == currentWord[i]) {
             var letterDivs = document.getElementsByClassName("letters-wrapper")[0].children;
             letterDivs[i].innerHTML = letter;
             foundLetter = true;
+            guessedLetters.push(letter);
+
+
+            if (lettersToGuess - 1 > 0) {
+                lettersToGuess--;
+            } else {
+                gameOver("win");
+            }
+
 
         }
     }
 
-    if(!foundLetter){
+
+    if (!foundLetter && !inGuessedLetters(letter)) {
         guessedLetters.push(letter);
-        console.log(guessedLetters);
         return false;
     } else {
         return true;
@@ -182,12 +263,44 @@ var checkLetter = (letter) => {
 };
 
 var updateMistakes = () => {
-    var mistakes = document.getElementById("mistakes-left");
-    mistakes.innerHTML = mistakesLeft;
+    if (mistakesLeft - 1 < 0) {
+        gameOver("loss");
+    } else {
+        mistakesLeft--;
+        var mistakes = document.getElementById("mistakes-left");
+        mistakes.innerHTML = mistakesLeft;
+    }
+
 }
 
-var checkGuessedLetters = () => {
-    //To be added
+var inGuessedLetters = (letter) => {
+    if (guessedLetters.indexOf(letter) == -1) {
+        return false;
+    } else {
+        console.log("Already guessed letter");
+        return true;
+    }
+};
+
+var createGameOverScene = (state) => {
+    var gameOverText = document.createElement("p");
+    var continueButton = document.createElement("button");
+
+    continueButton.innerHTML = "Continue";
+    continueButton.className = "continue-button button";
+    continueButton.addEventListener("click", () => {
+        createStartingScreen();
+    });
+
+    if(state == "win"){
+        gameOverText.innerHTML = `Congratulations! You've guessed <span class='strong'>${currentWord}!</span>`;
+    } else if (state == "loss"){
+        gameOverText.innerHTML = `Sorry! Your word was ${currentWord}.`;
+    }
+
+
+    appendToApp(gameOverText);
+    appendToApp(continueButton);
 };
 
 
